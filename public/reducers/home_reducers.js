24744807fc8ad2d5ecca391a3f92df4a768c4991
YZH <YZH>
2017-05-11 21:37:14 +0800

    git add --all

--- public/reducers/home_reducers.js
+++ public/reducers/home_reducers.js
@@ -1,12 +1,15 @@
 import {
-    CUR_COUNT
+    ADD_COUNT,
+    RED_COUNT
 } from '../actions/home_action.js';
 
 export const changeCount = (state = 0, action) => {
     console.log(action);
-    switch(action.type){
-        case CUR_COUNT:
-            return ++state
+    switch (action.type) {
+        case ADD_COUNT:
+            return ++state;
+        case RED_COUNT:
+            return --state;
         default:
             return state;
     }
